15776ece0bd1029cbc1a82da59c183d75e54fbaa
roomain <romain.crespel@gmail.com>
2024-06-04 06:30:14 +0200

    image interface

--- Engine/VkEngineImage.h
+++ Engine/VkEngineImage.h
@@ -5,22 +5,21 @@
 * @author Roomain
 ************************************************/
 #include <memory>
-#include <vulkan/vulkan.hpp>
-#include "vk_engine_globals.h"
+#include "VkImageInterface.h"
 
 class VkEngineDevice;
 
 #pragma warning(push)
 #pragma warning(disable : 4251)
 
-/*@brief represents an image*/
-class ENGINE_EXPORT VkEngineImage
+
+
+/*@brief represents an image manually created*/
+class ENGINE_EXPORT VkEngineImage : public VkImageInterface
 {
 	friend VkEngineDevice;
 private:
-	VkImage m_image = VK_NULL_HANDLE;					/*!< image data*/
 	VkDeviceMemory m_memory = VK_NULL_HANDLE;			/*!< image memory*/
-	VkImageView m_imageView = VK_NULL_HANDLE;			/*!< image view*/
 	VkImageCreateInfo m_createInfo;						/*!< image create info (contains size, format etc.)*/
 	std::weak_ptr<VkEngineDevice> m_device;				/*!< device which belong image*/
 	VkImageAspectFlags m_aspect = VK_IMAGE_ASPECT_NONE;	/*!< image view aspect*/
@@ -36,13 +35,10 @@ public:
 	virtual ~VkEngineImage();
 	void reset();
 	[[nodiscard]] bool createView(const VkImageAspectFlags a_aspectFlag);
-	[[nodiscard]] constexpr bool isValid()const noexcept { return m_image != VK_NULL_HANDLE; }
-	[[nodiscard]] constexpr uint32_t width() const noexcept { return m_createInfo.extent.width; }
-	[[nodiscard]] constexpr uint32_t height() const noexcept { return m_createInfo.extent.height; }
-	[[nodiscard]] constexpr uint32_t depth() const noexcept { return m_createInfo.extent.depth; }
-	[[nodiscard]] constexpr VkFormat format() const noexcept { return m_createInfo.format; }
-	[[nodiscard]] constexpr VkImage image() const noexcept { return m_image; }
-	[[nodiscard]] constexpr VkImageView imageView() const noexcept { return m_imageView; }
+	[[nodiscard]] constexpr uint32_t width() const noexcept final { return m_createInfo.extent.width; }
+	[[nodiscard]] constexpr uint32_t height() const noexcept final { return m_createInfo.extent.height; }
+	[[nodiscard]] constexpr uint32_t depth() const noexcept final { return m_createInfo.extent.depth; }
+	[[nodiscard]] constexpr VkFormat format() const noexcept final { return m_createInfo.format; }
 };
 
 #pragma warning(pop)
